d24f55ede0482ea9423b8858aaeef8f6e8525ca4
sangramdhurve <sangramdhurve96@gmail.com>
2023-05-08 15:32:45 +0530

    added web api data format conversion:

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import shutil
 from fastapi import FastAPI, File, UploadFile
 from typing import List
 from Analyze_dataset.DataAnalysis import analyze_dataset
+# Imports for Data conversion
+from typing import Optional
+from DataFormat_change import Coco_To_Yolo
+from DataFormat_change import Voc_To_Coco
+from DataFormat_change import Voc_To_Yolo
 
 app = FastAPI()
 
@@ -31,6 +36,25 @@ async def analyze(images: List[UploadFile] = File(...)):
     shutil.rmtree(temp_dir)
 
     return result
+
+
+
+@app.get("/convert/")
+async def convert_data(input_format: str, output_format: str, input_dir: str, output_dir: Optional[str] = None):
+    """
+    Converts a dataset from one format to another.
+    """
+    if input_format == 'coco' and output_format == 'yolo':
+        Coco_To_Yolo.coco_to_yolo(input_dir, output_dir)
+    elif input_format == 'voc' and output_format == 'coco':
+        Voc_To_Coco.voc_to_coco(input_dir, output_dir)
+    elif input_format == 'voc' and output_format == 'yolo':
+        Voc_To_Yolo.convert_voc_to_yolo(input_dir, output_dir)
+    else:
+        return {"message": "Unsupported conversion"}
+
+    return {"message": "Conversion completed"}
+
 
 # if __name__ == "__main__":
 #     import uvicorn
